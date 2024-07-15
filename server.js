3b324074522ae8a8964c5084a8f7f4b9b46554ad
Kinan GH <KinanGH98@gmail.com>
2024-07-15 13:42:50 +0300

    Server now won't re-send the incoming message back to its sender.

--- server.js
+++ server.js
@@ -4,26 +4,32 @@ const http = require('http');
 const port = process.env.PORT || 8080; // Use the PORT environment variable or default to 8080
 
 const server = http.createServer();
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({server});
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws) =>
+{
     console.log('Client connected');
 
-    ws.on('message', (message) => {
+    ws.on('message', (message) =>
+    {
         console.log(`Received message => ${message}`);
         // Broadcast to all clients
-        wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
+        wss.clients.forEach((client) =>
+        {
+            if (client !== ws && client.readyState === WebSocket.OPEN)
+            {
                 client.send(message);
             }
         });
     });
 
-    ws.on('close', () => {
+    ws.on('close', () =>
+    {
         console.log('Client disconnected');
     });
 });
 
-server.listen(port, () => {
+server.listen(port, () =>
+{
     console.log(`Server is listening on port ${port}`);
 });
